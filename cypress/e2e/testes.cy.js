2a163f07ff7ea132dff26fadc3515f787e9cec7e
Hibblonico <56742535+Hibblonico@users.noreply.github.com>
2023-05-10 17:02:19 -0300

    mais casos e relatorio

--- cypress/e2e/testes.cy.js
+++ cypress/e2e/testes.cy.js
@@ -14,6 +14,27 @@ describe("Criando cenario de teste do site Inatel", ()=>{
     it("Caso de teste: Acessando a página do Portal da Graduação",()=>{
       let info3 = Portal()
       })
+
+      it("Caso de teste: Acessando a página sobre Santa Rita do Sapucaí", ()=>{
+        let info = Cookie()
+        cy.get('#dropbtn > .fa').click()
+        cy.get(':nth-child(1) > .scroll-link').click()
+        cy.get('.backComImgFaixa6 > .wrap1396 > .wrapCont > .flexBoxGeral > .flexCol2 > .flexFaixas-col2 > p > .btn').click()
+        cy.get('.wrapSobrepoeDestaque > :nth-child(1) > .SJ-MB-15').should("contain.text", "Santa Rita")
+    })
+
+    it("Caso de teste: Acessando a página sobre Pesquisa e Inovação", ()=>{
+        let info = Cookie()
+        cy.get('#compartilhamosIdeias > .align-items-center > :nth-child(1) > .flexFaixas-col2 > p > .btn').click()
+        cy.visit('https://inatel.br/pesquisador/')
+        cy.get('.IconBox1-Pesquisa').should("have.text", "MESTRADO E DOUTORADO")
+    })
+    it("Caso de teste: Acessando a página sobre Notícias e Eventos futuros", ()=>{
+        let info = Cookie()
+        cy.get('.btn-verdeFundo').click()
+        cy.visit('https://inatel.br/eventos/')
+        cy.get('.wrap1200-padding-25 > .SJ-FZ-21').should("have.text", "EVENTOS")
+    })
     
     function Cookie(){
       let horas = new Date().getHours().toString()
